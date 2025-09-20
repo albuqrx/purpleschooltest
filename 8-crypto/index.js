c89fe800fa6ac9db80003f6ac29e076084ba7c8f
albuqrx <albuqrx@yandex.ru>
2025-09-20 15:01:01 +0300

    fix

--- 8-crypto/index.js
+++ 8-crypto/index.js
@@ -2,7 +2,32 @@
 
 let password = "password";
 
-function changeindex(index, array) {
+// function changeindex(index, array) {
+//     for (let i = 2; i < 5; i++) {
+//         const element = array[i];
+//         // console.log(element);
+//         if (i != 4) {
+//             array[i] = array.at(i - index);
+//             array[i - index] = element;
+//             index += 2;
+//         } else {
+//             index = 3;
+//             array[i] = array.at(i + index);
+//             array[i + index] = element;
+//         }
+//     }
+//     return array.join('');
+// }
+
+function crypto(pass) {
+    if (pass.length < 4) return "Пароль слишком короткий!";
+
+    let cryptoPass = "";
+    let index = 1;
+    let array = pass.split("");
+    console.log(array);
+
+    
     for (let i = 2; i < 5; i++) {
         const element = array[i];
         // console.log(element);
@@ -14,24 +39,11 @@ function changeindex(index, array) {
             index = 3;
             array[i] = array.at(i + index);
             array[i + index] = element;
-            
-            
         }
     }
-    return array.join('');
-}
 
-function crypto(pass) {
-    if (pass.length < 4) return "Пароль слишком короткий!";
+    cryptoPass = array.join('');
 
-    let cryptoPass = "";
-    let index = 1;
-    let array = pass.split("");
-    console.log(array);
-
-    cryptoPass = changeindex(index, array);
-
-    console.log(cryptoPass);
     return cryptoPass;
 }
 
@@ -41,12 +53,27 @@ function check(cryptoPass, pass) {
     let index = 1;
     let array = cryptoPass.split("");
 
-    const newPass = changeindex(index, array);
+    for (let i = 2; i < 5; i++) {
+        const element = array[i];
+        // console.log(element);
+        if (i != 4) {
+            array[i] = array.at(i - index);
+            array[i - index] = element;
+            index += 2;
+        } else {
+            index = 3;
+            array[i] = array.at(i + index);
+            array[i + index] = element;
+        }
+    }
+
+
+    const newPass = array.join('');
     console.log(newPass)
     return pass == newPass;
 
 }
 
 const cryptoPass = crypto(password);
-
-console.log(check(cryptoPass, password));
+console.log(cryptoPass == 'ssapdorw');
+// console.log(check(cryptoPass, password));
